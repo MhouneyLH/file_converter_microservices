2f692695d42949589f09684e878cbcc8cb6f076c
Lucas Hünniger <lucas.huenniger02@gmail.com>
2023-11-17 18:46:16 +0100

    feat: add sql init script

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ A simple microservice-project for converting video files to mp3 files that is or
 ## My notes
 
 - venv für Python verwenden -> für dieses Umgebung dann Dinge installieren
+- Database erstellen mit init-Skript: mysql -u root
+- einzelnen Befehl ausführen: mysql -u root -e "BEFEHL"
+- Database leeren: mysql -u root -e "DROP DATABASE auth"
+- User entfernen: mysql -u root -e "DROP USER auth_user@localhost"
---
+++ python/src/auth/init.sql
@@ -0,0 +1,19 @@
+/* THIS IS LIKE A SQL SCRIPT */
+/* user for the actual database */
+CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'auth_user123';
+
+CREATE DATABASE auth;
+
+/* this user will have the rights to connect to the API-Gateway */
+GRANT ALL PRIVILEGES ON auth.* TO 'auth_user'@'localhost';
+
+/* use this database */
+USE auth;
+
+CREATE TABLE user (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+INSERT INTO user(email, password) VALUES ('lucas@email.com', "Huhn123");
